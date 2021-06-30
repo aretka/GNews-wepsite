a80cd4c8e9e11ccc2af62e062c222b819f843f6e
aretask2@gmail.com <aretask2@gmail.com>
2021-06-30 12:07:46 +0300

    Modify project structure

---
+++ gnews-website/src/components/MainSections/ContentSection/ContentSection.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ContentSection = props => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default ContentSection;
---
+++ gnews-website/src/components/UI/SearchButton/SearchButton.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+import classes from './SearchButton.module.css'
+
+const SearchButton = props => {
+    return <button className={classes.button}></button>
+}
+
+export default SearchButton;
