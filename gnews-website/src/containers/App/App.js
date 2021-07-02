6810fa8d82a0b31b6ea5fc5ce2cdf44c75b6029d
aretask2@gmail.com <aretask2@gmail.com>
2021-07-02 15:59:25 +0300

    Add layout to app container

--- gnews-website/src/containers/App/App.js
+++ gnews-website/src/containers/App/App.js
@@ -1,11 +1,8 @@
 import './App.css';
+import Layout from '../../hoc/Layout/Layout'
 
 function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+  return <Layout />
 }
 
 export default App;
