69690a1a14805fdf6938d2bc42191b8286809773
aretask2@gmail.com <aretask2@gmail.com>
2021-07-02 15:57:51 +0300

    Add onClick listener

--- gnews-website/src/components/UI/SearchButton/SearchButton.js
+++ gnews-website/src/components/UI/SearchButton/SearchButton.js
@@ -3,7 +3,7 @@ import React from 'react'
 import classes from './SearchButton.module.css'
 
 const SearchButton = props => {
-    return <button className={classes.button}>{props.children}</button>
+    return <button className={classes.button} onClick={props.onClicked}>{props.children}</button>
 }
 
 export default SearchButton;
