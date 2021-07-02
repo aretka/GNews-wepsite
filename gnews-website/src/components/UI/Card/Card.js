b921bb3e1cfcaa1828329ca4fae321b5db30ceee
aretask2@gmail.com <aretask2@gmail.com>
2021-07-02 15:56:00 +0300

    Create card structure

--- gnews-website/src/components/UI/Card/Card.js
+++ gnews-website/src/components/UI/Card/Card.js
@@ -1,9 +1,27 @@
 import React from 'react'
+// import { Image } from 'react-native'
+
+import classes from './Card.module.css'
 
 const Card = (props) => {
+    const articleClickedListener = () => {
+
+    }
+
     return(
-        <div>
-            
+        <div className={classes.Card} onClick={articleClickedListener}>
+            <div className={classes.ImageContainer}>
+                <img  className={classes.Image} src={props.image}/>
+            </div>
+            <div className={classes.TitleContainer}>
+                <h3>{props.title}</h3>
+            </div>
+            <div className={classes.TextContainer}>
+                <p className={classes.Text}>{props.description}</p>
+            </div>
+            <div className={classes.TextContainer}>
+                <p className={classes.Published}>{props.publishedAt}</p>
+            </div>
         </div>
     )
 }
