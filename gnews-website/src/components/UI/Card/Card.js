d9b41e8d49795f2bd6fc928cef11966b3202cb83
aretask2@gmail.com <aretask2@gmail.com>
2021-07-03 13:25:30 +0300

    Fix image source from props

--- gnews-website/src/components/UI/Card/Card.js
+++ gnews-website/src/components/UI/Card/Card.js
@@ -1,27 +1,24 @@
 import React from 'react'
-// import { Image } from 'react-native'
 
 import classes from './Card.module.css'
 
 const Card = (props) => {
-    const articleClickedListener = () => {
-
-    }
-
     return(
-        <div className={classes.Card} onClick={articleClickedListener}>
-            <div className={classes.ImageContainer}>
-                <img  className={classes.Image} src={props.image}/>
-            </div>
-            <div className={classes.TitleContainer}>
-                <h3>{props.title}</h3>
-            </div>
-            <div className={classes.TextContainer}>
-                <p className={classes.Text}>{props.description}</p>
-            </div>
-            <div className={classes.TextContainer}>
-                <p className={classes.Published}>{props.publishedAt}</p>
-            </div>
+        <div className={classes.Card}> 
+            <a className={classes.Link} rel="noopener noreferrer" target="_blank" href={props.url}>
+                <div className={classes.ImageContainer}>
+                    <img  className={classes.Image} src={props.image}/>
+                </div>
+                <div className={classes.TitleContainer}>
+                    <h3>{props.title}</h3>
+                </div>
+                <div className={classes.TextContainer}>
+                    <p className={classes.Text}>{props.description}</p>
+                </div>
+                <div className={classes.TextContainer}>
+                    <p className={classes.Published}>{props.publishedAt}</p>
+                </div>
+            </a>
         </div>
     )
 }
