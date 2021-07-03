54b85196cea2457782f0d646750178a56bcfe1fb
aretask2@gmail.com <aretask2@gmail.com>
2021-07-03 14:14:12 +0300

    Add solution if no articles found

--- gnews-website/src/components/MainSections/ContentSection/ContentSection.js
+++ gnews-website/src/components/MainSections/ContentSection/ContentSection.js
@@ -7,7 +7,8 @@ import Card from '../../UI/Card/Card'
 class ContentSection extends Component {
     state = {
         enteredArticleTitle: '',
-        articleArray: []
+        articleArray: [],
+        showErrorMessage: null
     }
 
     articleTitleChangedHandler = (event) => {
@@ -17,24 +18,34 @@ class ContentSection extends Component {
     onSubmitHandler = (event) => {
         event.preventDefault()
 
-        const url = `https://gnews.io/api/v4/search?q=${this.state.enteredArticleTitle}%20iphone&token=96ee5b7acb258b56ea47bb823edd4f44&max=9`;
+        const spaceCount = this.state.enteredArticleTitle.split(' ').length - 1
+
+        if(spaceCount > 40) {
+
+        }
+
+        const url = `https://gnews.io/api/v4/search?q=${this.state.enteredArticleTitle}&token=96ee5b7acb258b56ea47bb823edd4f44&max=9`;
         fetch(url)
         .then(response => response.json())
         .then((jsonData) => {
-            console.log(jsonData.articles)
-            let numberOfArticles = 9
             this.setState({ articleArray: [] })
-            for(let i = 0; i < numberOfArticles; i++) {
-                const article = {
-                    id: i,
-                    image: jsonData.articles[i].image,
-                    title: jsonData.articles[i].title,
-                    description: jsonData.articles[i].description,
-                    publishedAt: jsonData.articles[i].publishedAt
+            
+            if(jsonData.articles.length == 0) {
+                this.setState({ articleArray: null })
+            } else {
+                for(let i = 0; i < jsonData.articles.length; i++) {
+                    const article = {
+                        id: i,
+                        image: jsonData.articles[i].image,
+                        title: jsonData.articles[i].title,
+                        description: jsonData.articles[i].description,
+                        publishedAt: jsonData.articles[i].publishedAt,
+                        url: jsonData.articles[i].url
+                    }
+                    this.setState(state => ({
+                        articleArray: [...state.articleArray, article]
+                    }))
                 }
-                this.setState(state => ({
-                    articleArray: [...state.articleArray, article]
-                }))
             }
         })
     }
@@ -42,7 +53,7 @@ class ContentSection extends Component {
     
 
     render () {
-        let articles = (
+        let articles = this.state.articleArray ? (
             <div className={classes.ArticleContainer}>
                 {this.state.articleArray.map((article) => (
                     <Card 
@@ -51,13 +62,22 @@ class ContentSection extends Component {
                         title={article.title}
                         description={article.description}
                         publishedAt={article.publishedAt}
+                        url={article.url}
                     />
                 ))}
             </div>
+        ) : (
+            <div>
+                <h1 className={classes.NotFoundTitle}>No articles found</h1>
+            </div>
         )
+
         return (
             
             <div>
+                {/* <div className={classes.SearchContainer}>
+
+                </div> */}
                 <div className={classes.SearchContainer}>
                     <input className={classes.Input} placeholder="Enter article title..." onChange={this.articleTitleChangedHandler}></input>
                 </div>
