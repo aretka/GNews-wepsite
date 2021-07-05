b1b1a05826cbca325c187a4f3a696722c581f50e
aretask2@gmail.com <aretask2@gmail.com>
2021-07-05 21:10:17 +0300

    Post article data to node js on article click

--- client/src/components/UI/Card/Card.js
+++ client/src/components/UI/Card/Card.js
@@ -1,12 +1,32 @@
 import React from 'react'
+import axios from 'axios'
 import { Card, Col } from 'react-bootstrap'
 
 import './Card.scss'
 
 const Article = (props) => {
+    const sendPostOfClickedArticle = () => {
+        const article = {
+            title: props.title,
+            description: props.description,
+            publishedAt: props.publishedAt,
+            imageURL: props.image,
+            originalLink: props.url
+        }
+        axios.post('http://localhost:9000/articles', JSON.stringify(article))
+            .then(() => console.log('Article title sent'))
+            .catch(error => {})
+    }
+    
     return(
         <Col md={4} className='Card'>
-            <a className='LinkToOriginalSite' rel="noopener noreferrer" target="_blank" href={props.url}>
+            <a 
+                className='LinkToOriginalSite' 
+                rel="noopener noreferrer" 
+                target="_blank" 
+                href={props.url}
+                onClick={sendPostOfClickedArticle}
+            >
                 <Card>               
                     <Card.Body>
                         <Card.Img src={props.image}/>
