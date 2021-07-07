2902713473f1eaf485b60adc0a75029af0837089
aretask2@gmail.com <aretask2@gmail.com>
2021-07-07 15:42:39 +0300

    Wrap up with context provider

--- client/src/containers/App/App.js
+++ client/src/containers/App/App.js
@@ -1,11 +1,17 @@
+import Header from '../../components/MainSections/Header/Header';
 import ContentSection from '../../components/MainSections/ContentSection/ContentSection';
 import Footer from '../../components/MainSections/Footer/Footer'
+import ArticleProvider from '../../articleContext/ArticleContext';
 
 function App() {
+
   return (
     <>
-      <ContentSection />
-      <Footer />
+      <ArticleProvider>
+        <Header />
+        <ContentSection />
+        <Footer />
+      </ArticleProvider>
     </>
   ) 
 }
